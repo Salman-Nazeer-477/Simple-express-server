fa9f12b824e81aa83356e3c7ac41edb0df367f3e
Salman <salmannazeer477@gmail.com>
2026-03-12 12:35:41 +0530

    fixed errors because of me forgetting to include fs and path modules

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const path = require('path')
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
@@ -6,8 +8,18 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/date', (req, res) => {
-  res.send(Date());
+app.get('/data', (req, res) => {
+  const filePath = path.join(__dirname, 'data.txt');
+
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Error reading the file');
+      return;
+    }
+    
+    res.send(data);
+  });
 });
 
 app.listen(port, () => {
